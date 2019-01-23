09923b40d7262b789dc85698875dfcec20f94976
Sharan <sharan@onlinerti.com>
2019-01-23 19:10:53 +0530

    0.0.2

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+
+var s_node_js_utils = require('s_node_js_utils')
+
+exports.formatIndianRupee = function(rupee) {
+	if (s_node_js_utils.isEmptyString(rupee)) {
+		return ""
+	}
+
+	rupee = s_node_js_utils.extractNumberFromString(rupee)
+
+	var splitDecimalArray = rupee.split(".")
+
+	var rupeeRound = splitDecimalArray[0]
+
+
+	console.log(rupeeRound)
+
+	var processedFormat = ""
+
+
+	if (rupeeRound.length <= 3) {
+		processedFormat = rupeeRound
+	} else {
+
+		
+		processedFormat = rupeeRound.substring(rupeeRound.length - 3, rupeeRound.length);
+		var addComma = true
+		for (var i = rupeeRound.length - 4; i >= 0; i--) {
+			if (addComma) {
+				processedFormat = rupeeRound.charAt(i) + "," + processedFormat
+				addComma = false
+			} else {
+				processedFormat = rupeeRound.charAt(i) + processedFormat
+				addComma = true
+			}
+		}
+	}
+
+	if (splitDecimalArray.length >= 2) {
+		return processedFormat + "." + splitDecimalArray[1]
+	}
+
+	return processedFormat
 }
+
+
+
---
+++ test.js
@@ -0,0 +1,2 @@
+var index = require('./index')
+console.log(index.formatIndianRupee("12345678900000.34333"))
